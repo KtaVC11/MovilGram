470a6109f0fc078d10ef8550cdcbd3e14a59067a
KtaVC11 <55267352+KtaVC11@users.noreply.github.com>
2020-05-07 11:19:47 -0600

    Initial commit

---
+++ app/src/main/java/com/example/movilgram/ReferenciaActivity.java
@@ -0,0 +1,14 @@
+package com.example.movilgram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReferenciaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_referencia);
+    }
+}
