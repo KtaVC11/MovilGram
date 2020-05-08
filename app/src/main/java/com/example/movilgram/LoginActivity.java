b769fad18b609c6585d8ea7eef9be7a07e377a18
KtaVC11 <55267352+KtaVC11@users.noreply.github.com>
2020-05-08 11:50:54 -0600

    Disenio

--- app/src/main/java/com/example/movilgram/LoginActivity.java
+++ app/src/main/java/com/example/movilgram/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.movilgram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.movilgram.View.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goCreateAccount(View view){
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/movilgram/View/CreateAccountActivity.java
+++ app/src/main/java/com/example/movilgram/View/CreateAccountActivity.java
@@ -1,6 +1,7 @@
 package com.example.movilgram.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -12,5 +13,14 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.toolbar_title_createaccount),true);//llamar un recurso, en este caso string y se llama al string que se creo, el true para que se vea
+
+    }
+
+    public void showToolbar(String title,boolean upButton){ //recibe un titulo, la mayoria y algunos botones
+        Toolbar toolbar =(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar); //para que se vea bien en versiones anteriores
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);//en caso de que tenga boton para que se vea el upButton osea un boton arriba en la jerarquia
     }
 }
